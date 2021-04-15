86f78a08053d2e973c23325ee0d95d4da7530712
Cesar Lopez <colc.1608@gmail.com>
2021-04-15 00:03:07 -0500

    subir fuente angular 11 de guia & subir proyecto CRUD funcional .. falta editar y eliminar

---
+++ bcp-reactive-colc-frontend/src/app/models/tutorial.model.ts
@@ -0,0 +1,9 @@
+export class Tutorial {
+  id?: any;
+  montoEnviado?: number;
+  montoRecibido?: number;
+  monedaOrigen?: string;
+  monedaDestino?: string;
+  tipoCambio?: number;
+  tipoOperacion?: string;
+}
---
+++ bcp-reactive-colc-frontend/src/app/models/tutorialRequest.model.ts
@@ -0,0 +1,6 @@
+export class Tutorial {
+  montoEnviado?: number;
+  tipoOperacion?: string;
+  monedaOrigen?: string;
+  monedaDestino?: string;
+}
